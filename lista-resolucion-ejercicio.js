4ae8137188ee9eb10ebd71dc025fe2a5b4eda2a2
gonzalotorres01 <lucho31109@gmail.com>
2023-03-17 17:20:48 -0300

    ejercicio completado

---
+++ lista-resolucion-ejercicio.js
@@ -0,0 +1,13 @@
+const lista = [
+    "gonzalo",
+    22,
+    true,
+    new Date(2000, 06, 15),
+    { 
+        titulo: "Viaje al centro de la tierra",
+        autor: "Julio Verne",
+        fecha: new Date(1864, 10, 25),
+        url: "https://www.argentina.gob.ar/sites/default/files/verne_julio_-_viaje_al_centro_de_la_tierra.pdf"
+      },
+  ];
+  console.log(lista)
