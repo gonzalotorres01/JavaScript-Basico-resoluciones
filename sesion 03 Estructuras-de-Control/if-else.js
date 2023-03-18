898c087d42f6c570c91eb36fa01f7a65de2ffc83
gonzalotorres01 <lucho31109@gmail.com>
2023-03-18 15:18:50 -0300

    seguimos estudiando

---
+++ sesion 03 Estructuras-de-Control/if-else.js
@@ -0,0 +1,12 @@
+// bifurcaciones if... else
+
+let saldo = 10;
+let efectivo = 15;
+if (true){
+    console.log("es verdadero");
+}
+if (false){
+    console.log("es verdadero");
+} else {
+    console.log("no es verdadero");
+}
